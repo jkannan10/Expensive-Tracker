803f5fe909e123d044740b7e1d3789e20a0de88b
Jayakannan <116925701+jkannan10@users.noreply.github.com>
2023-09-17 07:24:57 +0530

    Add files via upload

---
+++ styledComponents.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: sans-serief;
+  margin: 30px 0 10px;
+`;
+export const Header = styled.span`
+  font-size: 25px;
+  font-weight: bold;
+`;
